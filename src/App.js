a744a83f1db8f8443f9a63b0f3e4e73304b837f0
suibingyue <suibingyue@beiquan.org>
2018-12-29 00:14:09 +0800

    更新课件

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React, {Component} from 'react';
-import {Container} from 'semantic-ui-react';
+import PubSub from "pubsub-js";
 import './App.css';
-import LoginForm from './view/Login'
-
+import LoginForm from './view/login/login'
+import Wallet from './view/wallet/wallet'
+import {Container} from "semantic-ui-react";
 
 class App extends Component {
+    state = {
+        login: false,
+        loading: false,
+        loginEvent: '',
+        wallets: []
+    }
+
+    componentWillMount() {
+        let loginEvent = PubSub.subscribe("onLoginSucc", this.onLoginSucc)
+        this.setState({loginEvent})
+    }
+
+    onLoginSucc = (msg, data) => {
+        console.log("登陆成功")
+        console.log(data)
+        this.setState({
+            login: true,
+            wallets: data
+        })
+    }
+
+    componentWillUnmount() {
+        PubSub.unsubscribe(this.state.loginEvent)
+    }
+
     render() {
+        let {login} = this.state
+        let content = login ? <Wallet wallets={this.state.wallets}/> : <LoginForm/>
         return (
-            <div className="App-header">
-                <Container>
-                    <LoginForm />
-                </Container>
-            </div>
+            <Container>
+                {content}
+            </Container>
         );
     }
 }
